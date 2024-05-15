ac50fc3c96a4a4dc6e6ab6eff062909882f89527
marcelomsousa-utexas <marcelo.sousa-utexas@utexas.edu>
2024-05-15 02:29:55 -0300

    remove shiny and shinyjs from required package

--- R/AuditSampling.R
+++ R/AuditSampling.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   #loadDependentLibraries()
-  required_packages <- c("tidyverse", "readxl", "ggplot2", "shiny", "shinyjs")
+  required_packages <- c("tidyverse", "readxl", "ggplot2")
 
   for (pkg in required_packages) {
     suppressMessages({
